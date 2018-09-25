74a428c57d5ad3a0b46de3295828e4df91448f58
Jhoan Saavedra <Jhoan Saavedra@DESKTOP-JGA2LGN>
2018-09-25 18:35:00 -0500

    Versión 0.01

--- Algorithms/src/fibonacci_kth_nth.java
+++ Algorithms/src/fibonacci_kth_nth.java
@@ -2,11 +2,7 @@
 import java.io.IOException;
 import java.math.BigInteger;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 /**
  *
  * @author Jhoan Saavedra
